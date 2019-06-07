3494fa5f27587345c1e4b5f52d243c4fdd659859
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 13:23:49 +1000

    Switch over to using gem

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,3 @@
-require 'scraperwiki'
-require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
+require "icon_scraper"
 
-scrape_icon_rest_xml("http://planning.randwick.nsw.gov.au/Pages/XC.Track.Advanced/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=217&o=xml")
+IconScraper.rest_xml("http://planning.randwick.nsw.gov.au/Pages/XC.Track.Advanced/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=217&o=xml")
